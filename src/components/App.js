f93905f42a8af9f8acf8d5a6873f60614bed7cc1
Anna <jingyu.chen.2020@gmail.com>
2021-08-04 08:38:26 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { TOKEN_KEY } from '../constants/constants';
+
+import Header from './Header';
+import Main from './Main';
+
+class App extends Component {
+  state = {
+    signedIn: localStorage.getItem(TOKEN_KEY) ? true : false
+  };
+
+  render() {
+    const signedInSuccess = (token) => {
+      this.setState({signedIn: true});
+      localStorage.setItem(TOKEN_KEY, token); // improve by add a hash function.
+    }
+    
+    const signedOutSuccess = () => {
+      this.setState({signedIn: false});
+      localStorage.removeItem(TOKEN_KEY);
+    }
+
+    const { signedIn } = this.state;
+    return (
+      <div>
+        <Header signedIn={signedIn} signedOutSuccess={signedOutSuccess}/>
+        <Main signedIn={signedIn} signedInSuccess={signedInSuccess}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import * as urlPaths from '../constants/paths';
+
+function Main(props) {
+    const { signedIn, signedInSuccess } = props;
+
+    const showSignIn = () => {
+        return signedIn ? (
+          <Redirect to={urlPaths.MY_PROFILE_PATH}/>
+        ) : (
+          <div>signIn</div>
+        );
+    };
+
+    const showMyProfile = () => {
+        return signedIn ? <div>myProfile</div> : <Redirect to={urlPaths.SIGN_IN_PATH} />;
+    };
+
+    return (
+        <div>
+            <Switch>
+                <Route path={urlPaths.START_PATH} exact render={showSignIn} />
+                <Route path={urlPaths.SIGN_IN_PATH} render={showSignIn} />
+                <Route path={urlPaths.SIGN_UP_PATH}>signUp</Route>
+                <Route path={urlPaths.FOOD_TRACKER_PATH}>foodTrancker</Route>
+                <Route path={urlPaths.REACTION_TRACKER_PATH}>reactionTracker</Route>
+                <Route path={urlPaths.ALLERGEN_ANALYSIS_PATH}>allergenAnalysis</Route>
+                <Route path={urlPaths.MY_PROFILE_PATH} render={showMyProfile} />
+            </Switch>
+        </div>
+    );
+}
+
+export default Main;
