92364cc9b0cbce323df0875348462da246ed4cbd
sihuangls <540600813@qq.com>
2021-08-13 19:49:23 -0400

    ReactionTracker-Fu_Guo

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,52 @@
-import React, { Component } from 'react';
-import { TOKEN_KEY } from '../constants/constants';
-
+import React, {Component} from 'react';
+import {TOKEN_KEY} from '../constants/constants';
+import "../styles/App.css"
 import Header from './Header';
 import Main from './Main';
 
 class App extends Component {
+
+
   state = {
-    signedIn: localStorage.getItem(TOKEN_KEY) ? true : false
+    // signedIn: localStorage.getItem(TOKEN_KEY) ? true : false
+    signedIn: true
   };
 
+
   render() {
+
+
     const signedInSuccess = (token) => {
-      this.setState({signedIn: true});
+
       localStorage.setItem(TOKEN_KEY, token); // improve by add a hash function.
-    }
-    
-    const signedOutSuccess = () => {
-      this.setState({signedIn: false});
-      localStorage.removeItem(TOKEN_KEY);
+      this.setState({signedIn: true});
+
     }
 
-    const { signedIn } = this.state;
+
+    const signedOutSuccess = () => {
+      localStorage.removeItem(TOKEN_KEY);
+      this.setState({signedIn: false});
+
+    }
+
+
+    const {signedIn} = this.state;
+
+
     return (
-      <div>
-        <Header signedIn={signedIn} signedOutSuccess={signedOutSuccess}/>
-        <Main signedIn={signedIn} signedInSuccess={signedInSuccess}/>
-      </div>
+
+
+        <div className="App">
+          <Header signedIn={signedIn} signedOutSuccess={signedOutSuccess}/>
+          <Main signedIn={signedIn} signedInSuccess={signedInSuccess}/>
+        </div>
     );
+
+
   }
+
+
 }
 
 export default App;
