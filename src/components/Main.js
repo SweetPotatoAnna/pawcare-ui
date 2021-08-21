86c5b235be0c703e2887bd2aff7a966faef8b25c
sihuangls <540600813@qq.com>
2021-08-21 11:57:12 -0400

    ReactionTracker

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Route, Switch, Redirect} from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import * as urlPaths from '../constants/paths';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
-import reactionTracker from "./reactionTracker";
+import ReactionTracker from "./ReactionTracker";
 
 
 function Main(props) {
-    const {signedIn, signedInSuccess} = props;
+    const { signedIn, signedInSuccess } = props;
 
     const showSignIn = () => {
         return signedIn ? (
@@ -18,27 +18,26 @@ function Main(props) {
         );
     };
 
-
     const showMyProfile = () => {
-        return signedIn ? <div>myProfile</div> : <Redirect to={urlPaths.SIGN_IN_PATH}/>;
+        return signedIn ?  <Redirect to={urlPaths.MY_PROFILE_PATH}/>: <Redirect to={urlPaths.SIGN_IN_PATH} />;
     };
 
+    const showReactionTracker = () => {
+        return signedIn ? <ReactionTracker /> : <Redirect to={urlPaths.SIGN_IN_PATH} />;
+    };
 
     return (
         <div>
             <Switch>
-                <Route path={urlPaths.START_PATH} exact render={showSignIn}/>
-
-                <Route path={urlPaths.SIGN_IN_PATH} render={showSignIn}/>
-
+                <Route path={urlPaths.START_PATH} exact render={showSignIn} />
+                <Route path={urlPaths.SIGN_IN_PATH} render={showSignIn} />
                 <Route path={urlPaths.SIGN_UP_PATH} component={SignUp}/>
 
 
                 <Route path={urlPaths.FOOD_TRACKER_PATH}>foodTrancker</Route>
-                <Route path={urlPaths.REACTION_TRACKER_PATH}render={reactionTracker}/>
+                <Route path={urlPaths.REACTION_TRACKER_PATH}render={showReactionTracker}/>
                 <Route path={urlPaths.ALLERGEN_ANALYSIS_PATH}>allergenAnalysis</Route>
-                <Route path={urlPaths.MY_PROFILE_PATH} render={showMyProfile}/>
-
+                <Route path={urlPaths.MY_PROFILE_PATH} render={showMyProfile} />
             </Switch>
         </div>
     );
