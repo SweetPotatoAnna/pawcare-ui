5eb393d80e8668194ca2399636bc5059ec5d36e5
Anna <jingyu.chen.2020@gmail.com>
2021-08-21 09:13:22 -0700

    test 1

--- src/components/AllergenAnalysis.js
+++ src/components/AllergenAnalysis.js
@@ -3,7 +3,7 @@ import React from 'react';
 function AllergenAnalysis(props) {
     return (
         <div>
-            AllergenAnalysis
+            AllergenAnalysis test
         </div>
     );
 }
