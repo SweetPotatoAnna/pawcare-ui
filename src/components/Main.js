6bb79f657d646f8e9e9c9ad93533a0871b5c5419
DimoJ <alneders@gmail.com>
2021-08-22 01:44:20 -0500

    Food tracker update last

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,7 @@ import SignIn from './SignIn';
 import MyProfile from "./MyProfile";
 import AllergenAnalysis from './AllergenAnalysis';
 import ReactionTracker from './ReactionTracker';
+import FoodTracker from "./FoodTracker";
 
 function Main(props) {
     const { signedIn, signedInSuccess } = props;
@@ -28,6 +29,10 @@ function Main(props) {
         return signedIn ? <AllergenAnalysis /> : <Redirect to={urlPaths.SIGN_IN_PATH} />;
     };
 
+    const showFoodTracker = () => {
+        return signedIn ? <FoodTracker /> : <Redirect to={urlPaths.SIGN_IN_PATH} />;
+    };
+
     return (
         <div class="main">
             <Switch>
@@ -35,7 +40,7 @@ function Main(props) {
                 <Route path={urlPaths.SIGN_IN_PATH} render={showSignIn} />
                 <Route path={urlPaths.SIGN_UP_PATH} component={SignUp}/>
 
-                <Route path={urlPaths.FOOD_TRACKER_PATH}>foodTrancker</Route>
+                <Route path={urlPaths.FOOD_TRACKER_PATH} render={showFoodTracker}/>
                 <Route path={urlPaths.REACTION_TRACKER_PATH} render={showReactionTracker} />
                 <Route path={urlPaths.ALLERGEN_ANALYSIS_PATH} render={showAllergenAnalysis} />
                 <Route path={urlPaths.MY_PROFILE_PATH} render={showMyProfile} />
